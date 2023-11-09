86d36dba545d1d648aa4a609ade52d7f7c4c937b
Sameer Sannidhanam <110780161+sameersannidhanam@users.noreply.github.com>
2023-11-09 22:35:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { D } from "./components/D";
+function App() {
+  return (
+    <div className="App">
+      <D />
+    </div>
+  );
+}
+
+export default App;
